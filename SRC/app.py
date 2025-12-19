c4796b043d1a7cfd29dded22283571cb4343cbe5
Haylize <elise.prgnt@gmail.com>
2025-12-19 17:49:14 +0100

    dashboard app

---
+++ SRC/app.py
@@ -0,0 +1,16 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Dashboard Immobilier",
+    layout="wide"
+)
+
+st.title("🏠 Dashboard Immobilier")
+st.markdown(
+    """
+    Utilisez le menu de gauche pour naviguer :
+    - 🏠 Accueil
+    - 🧪 Méthodologie
+    - 📊 Analyse
+    """
+)
